f77d0f5f17987d14f75b8972c05df4aad8c07f58
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-21 13:40:56 +0000

    Add files via upload

--- omega_find.py
+++ omega_find.py
@@ -10,7 +10,6 @@ import magic
 import codecs
 import asyncio
 from aiomultiprocess import Pool
-import omega_find_sysargv
 import prescan
 import chunk_handler
 import pathlib
@@ -18,26 +17,14 @@ import aiofiles
 import omega_find_help
 import re
 
-
-def get_dt() -> str:
-    return str(datetime.now()).replace(':', '-').replace('.', '-').replace(' ', '_')
+learn_seen_before = []
 
 
-def pre_scan_handler(_target: str):
+def pre_scan_handler(_target: str) -> list:
     scan_results = prescan.scan(path=_target)
     _files = scan_results[0]
     _x_files = scan_results[1]
     return _files, _x_files
-
-
-def read_db():
-    _recognized_files, _suffixes = [], []
-    if os.path.exists(_db_recognized_files):
-        if learn is False:
-            _recognized_files, _suffixes = asyncio.run(async_read_definitions(fname=_db_recognized_files, digits=_digits))
-        elif learn is True:
-            _recognized_files, _suffixes = asyncio.run(async_read_definitions_learn_mode(fname=_db_recognized_files))
-    return _recognized_files, _suffixes
 
 
 def get_suffix(file: str) -> str:
@@ -57,87 +44,75 @@ def file_sub_ops(_bytes: str) -> str:
     return buff
 
 
-async def read_bytes(file: str, _buffer_max: int) -> bytes:
+async def read_bytes(file: str, _buffer_size: int) -> bytes:
     async with aiofiles.open(file, mode='rb') as handle:
-        _bytes = await handle.read(_buffer_max)
+        _bytes = await handle.read(_buffer_size)
+        await handle.close()
     return await asyncio.to_thread(file_sub_ops, _bytes)
 
 
 async def scan_learn_check(suffix: str, buffer: bytes, _recognized_files):
+    global learn_seen_before
+    digi_str = r'[0-9]'
+    buffer = re.sub(digi_str, '', str(buffer))   # todo optional digit-less matching
     if [suffix, buffer] not in _recognized_files:
-        return [suffix, buffer]
+        if [suffix, buffer] not in learn_seen_before:
+            learn_seen_before.append([suffix, buffer])
+            return [suffix, buffer]
 
 
-async def scan_learn(file: str, _recognized_files: list, _buffer_max: int) -> list:
+async def scan_learn(file: str, _recognized_files: list, _buffer_size: int) -> list:
     try:
-        buffer = await read_bytes(file, _buffer_max)
+        buffer = await read_bytes(file, _buffer_size)
         suffix = await asyncio.to_thread(get_suffix, file)
-        return await scan_learn_check(suffix, buffer, _recognized_files)
+        x = await scan_learn_check(suffix, buffer, _recognized_files)
+        return x
     except:
         pass
 
 
-async def de_scan_check(file: str, suffix: str, buffer: bytes, _recognized_files, digits: False):
-    tmp_buffer = buffer
-    if digits is True:
-        digi_str = r'[0-9]'
-        tmp_buffer = re.sub(digi_str, '', str(buffer))
-    if [suffix, tmp_buffer] not in _recognized_files:
+async def de_scan_check(file: str, suffix: str, buffer: bytes, _recognized_files):
+    digi_str = r'[0-9]'
+    buffer = re.sub(digi_str, '', str(buffer))   # todo optional digit-less matching
+    if [suffix, buffer] not in _recognized_files:
         return [file, suffix, buffer]
 
 
-async def de_scan(file: str, _recognized_files: list, _buffer_max: int, digits: False) -> list:
+async def de_scan(file: str, _recognized_files: list, _buffer_size: int) -> list:
+    global learn_seen_before
     try:
-        buffer = await read_bytes(file, _buffer_max)
+        buffer = await read_bytes(file, _buffer_size)
         suffix = await asyncio.to_thread(get_suffix, file)
-        return await de_scan_check(file, suffix, buffer, _recognized_files, digits)
+        return await de_scan_check(file, suffix, buffer, _recognized_files)
     except:
         pass
 
 
 async def entry_point_learn(chunk: list, **kwargs) -> list:
     _recognized_files = kwargs.get('files_recognized')
-    buffer_max = int(kwargs.get('buffer_max'))
-    return [await scan_learn(item, _recognized_files, buffer_max) for item in chunk]
+    _buffer_size = int(kwargs.get('buff_size'))
+    return [await scan_learn(item, _recognized_files, _buffer_size) for item in chunk]
 
 
 async def entry_point_de_scan(chunk: list, **kwargs) -> list:
     _recognized_files = kwargs.get('files_recognized')
-    buffer_max = int(kwargs.get('buffer_max'))
-    digits = bool(kwargs.get('digits'))
-    return [await de_scan(item, _recognized_files, buffer_max, digits) for item in chunk]
+    _buffer_size = int(kwargs.get('buff_size'))
+    return [await de_scan(item, _recognized_files, _buffer_size) for item in chunk]
 
 
 async def main(_chunks: list, _multiproc_dict: dict, _mode: str):
-    if mode == '--learn':
-        async with Pool() as pool:
+    global learn_seen_before
+    async with Pool() as pool:
+        if mode == '--learn':
+            learn_seen_before = []
             _results = await pool.map(entry_point_learn, _chunks, _multiproc_dict)
-    elif mode == '--de-scan':
-        async with Pool() as pool:
+        elif mode == '--de-scan':
             _results = await pool.map(entry_point_de_scan, _chunks, _multiproc_dict)
     return _results
 
 
-async def async_read_definitions(fname: str, digits: False):
-    async with aiofiles.open(fname, mode='r', encoding='utf8') as handle:
-        _data = await handle.read()
-    _data = _data.split('\n')
-    _file_recognition_store = []
-    _suffixes = []
+async def async_read_definitions(fname):
     digi_str = r'[0-9]'
-    for datas in _data:
-        idx = datas.find(' ')
-        suffix = datas[:idx]
-        buffer = datas[idx+1:]
-        if digits is True:
-            buffer = re.sub(digi_str, '', buffer)
-        _file_recognition_store.append([suffix, buffer])
-        if suffix not in _suffixes:
-            _suffixes.append(suffix)
-    return _file_recognition_store, _suffixes
-
-
-async def async_read_definitions_learn_mode(fname: str):
     async with aiofiles.open(fname, mode='r', encoding='utf8') as handle:
         _data = await handle.read()
     _data = _data.split('\n')
@@ -147,6 +122,7 @@ async def async_read_definitions_learn_mode(fname: str):
         idx = datas.find(' ')
         suffix = datas[:idx]
         buffer = datas[idx+1:]
+        buffer = re.sub(digi_str, '', buffer)  # todo optional digitless matching
         _file_recognition_store.append([suffix, buffer])
         if suffix not in _suffixes:
             _suffixes.append(suffix)
@@ -183,26 +159,37 @@ if __name__ == '__main__':
 
     else:
         # Notice: Requires the aiomultiprocess pool file that I personally modified or this will not work.
+        # WARNING: ensure sufficient ram/page-file/swap if changing buffer_size. ensure _proc_max suits your system.
+        _db_recognized_files = './db/database_file_recognition.txt'
+        modes = ['--learn', '--de-scan']
+        mode = ''
+        for m in modes:
+            if m in sys.argv:
+                mode = m
+        _target = sys.argv[sys.argv.index(mode)+1]
+        _proc_max = int(sys.argv[sys.argv.index('--proc-max')+1])
+        _buffer_size = int(sys.argv[sys.argv.index('--buffer-max')+1])
+        if '--database' in sys.argv:
+            _db_recognized_files = sys.argv[sys.argv.index('--database') + 1]
 
-        mode, learn, de_scan = omega_find_sysargv.mode()
-        _target = omega_find_sysargv.target(mode)
-        _chunk_max = omega_find_sysargv.chunk_max()
-        _buffer_max = omega_find_sysargv.buffer_max()
-        _db_recognized_files = omega_find_sysargv.database()
-        _digits = omega_find_sysargv.digits()
-        verbose = omega_find_sysargv.verbosity()
+        verbose = False
+        if '-v' in sys.argv:
+            verbose = True
 
         if os.path.exists(_target):
             print('\n[OmegaFind v2] Version 2. Multi-processed async for better performance.')
 
-            dt = get_dt()
+            # create datetime tag
+            dt = str(datetime.now()).replace(':', '-').replace('.', '-').replace(' ', '_')
 
-            # read database
-            recognized_files, suffixes = read_db()
+            # read recognized files
+            recognized_files, suffixes = [], []
+            if os.path.exists(_db_recognized_files):
+                recognized_files, suffixes = asyncio.run(async_read_definitions(fname=_db_recognized_files))
             if verbose is True:
                 print(f'[Recognized Buffers] {len(recognized_files)}')
                 print(f'[Known Suffixes] {len(suffixes)}')
-                # print(recognized_files)
+            # print(recognized_files)
 
             # pre-scan
             print('[Pre-Scanning] ..')
@@ -216,14 +203,13 @@ if __name__ == '__main__':
             asyncio.run(async_write_scan_results(*x_files, file='pre_scan_x_files_'+dt+'.txt', _dt=dt))
 
             # chunk data ready for async multiprocess
-            chunks = chunk_handler.chunk_data(files, _chunk_max)
+            chunks = chunk_handler.chunk_data(files, _proc_max)
             if verbose is True:
                 print('[Expected Number Of Chunks]', len(chunks))
 
             # prepare a dictionary of useful things for each child process
             multiproc_dict = {'files_recognized': recognized_files,
-                              'buffer_max': _buffer_max,
-                              'digits': _digits}
+                              'buff_size': _buffer_size}
 
             # run the async multiprocess operation(s)
             print(f'[Scanning Target] ..')
@@ -236,14 +222,12 @@ if __name__ == '__main__':
             print(f'[Results] {len(results)}')
             # print('[Results]', results)
 
-            # post scan: learn
             if mode == '--learn':
                 print(f'[New Definitions] {len(results)}')
                 if len(results) >= 1:
                     print('[Updating Definitions] ..')
                     asyncio.run(async_write_definitions(*results, file=_db_recognized_files))
 
-            # post scan: deobfuscation
             elif mode == '--de-scan':
                 print(f'[Unrecognized Files] {len(results)}')
                 if len(results) >= 1:
@@ -254,6 +238,7 @@ if __name__ == '__main__':
             print('')
 
         else:
+            # if os.path.exists(_target) and os.path.exists(_db_recognized_files):
             print('[Invalid Input]')
             if not os.path.exists(_target):
                 print('[Invalid Target]', _target)
