c43525c9171af804e76cdbe7c3323a90202dcc9d
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-22 01:54:30 +0000

    Add files via upload

--- omega_find_help.py
+++ omega_find_help.py
@@ -2,8 +2,8 @@
 
 def omega_help():
     print('')
-    print(' [OmegaFind v2]     [Version 2. Multi-processed async for better performance]')
-    print('                    [Forensics tool. Search differently]')
+    print(' [OmegaFind v2]  [Version 2. Multi-processed async for better performance]')
+    print('                 [Forensics tool. Search differently]')
     print('')
     print(' [--learn]             [Learn]                    [Scans and learns from specified target location]')
     print(' [--chunk-max]         [Chunk Max]                [Maximum items in each chunk. (Default 16)]')
