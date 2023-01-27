46faff33ff973bc955170d3469e18da6bfeb28bb
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-27 00:44:29 +0000

    Add files via upload

--- handler_results.py
+++ handler_results.py
@@ -1,3 +1,6 @@
+import asyncio
+import handler_file
+
 
 def result_handler(_results: list):
     if len(_results) <= 12:
@@ -13,3 +16,25 @@ def result_handler(_results: list):
                 i_result += 1
             else:
                 break
+
+
+def post_scan_results(_results: list, _db_recognized_files: list,
+                      _learn_bool: bool, _de_scan_bool: bool, _type_scan_bool: bool, _dt: str):
+    if len(_results) >= 1:
+        if _learn_bool is True:
+            print(f'[New Definitions] {len(_results)}')
+            print('[Updating Definitions] ..')
+            asyncio.run(handler_file.write_definitions(*_results, file=_db_recognized_files))
+            asyncio.run(handler_file.clean_database(fname=_db_recognized_files))
+        elif _de_scan_bool is True:
+            print(f'[Unrecognized Files] {len(_results)}')
+            print('[Writing Scan Results] ..')
+            asyncio.run(handler_file.write_scan_results(*_results, file='scan_results__' + _dt + '.txt', _dt=_dt))
+            result_handler(_results=_results)
+        elif _type_scan_bool is True:
+            print(f'[Found Files] {len(_results)}')
+            print('[Writing Scan Results] ..')
+            asyncio.run(handler_file.write_scan_results(*_results, file='scan_results__' + _dt + '.txt', _dt=_dt))
+    else:
+        print('[Zero Results]')
+    print('')
