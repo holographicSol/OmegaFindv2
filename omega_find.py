316ecdf4cc50124e01769d4e76391800c28ecf60
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-30 10:03:00 +0000

    Add files via upload

--- omega_find.py
+++ omega_find.py
@@ -39,7 +39,7 @@ async def extract_type_scan(_buffer: bytes, _file: str, _buffer_max: int, _recog
                             _type_suffix: list) -> list:
     _result = [_file]
     _tmp = '.\\tmp\\'+str(randStr())+'\\'
-    extraction = handler_file.extract_nested_compressed(file=_file, temp_directory=_tmp)
+    extraction = await asyncio.to_thread(handler_file.extract_nested_compressed, file=_file, temp_directory=_tmp)
     if extraction is True:
         sub_files = scanfs.scan(_tmp)
         sub_files = handler_chunk.un_chunk_data(sub_files, depth=1)
@@ -56,7 +56,7 @@ async def extract_type_scan(_buffer: bytes, _file: str, _buffer_max: int, _recog
 async def extract_de_scan(_buffer: bytes, _file: str, _buffer_max: int, _recognized_files: list) -> list:
     _result = [_file]
     _tmp = '.\\tmp\\'+str(randStr())+'\\'
-    extraction = handler_file.extract_nested_compressed(file=_file, temp_directory=_tmp)
+    extraction = await asyncio.to_thread(handler_file.extract_nested_compressed, file=_file, temp_directory=_tmp)
     if extraction is True:
         sub_files = scanfs.scan(_tmp)
         sub_files = handler_chunk.un_chunk_data(sub_files, depth=1)
