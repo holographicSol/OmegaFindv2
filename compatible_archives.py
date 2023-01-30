0c1fd673ab7b9645f1ecf4d4ba361ea5d8df3ea7
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-01-30 21:19:45 +0000

    Add files via upload

--- compatible_archives.py
+++ compatible_archives.py
@@ -1,2 +1,2 @@
-# Note: this already accounts for many archive suffixes. an epub for example are zip archives
+# Note: this already accounts for many archive suffixes. an epub for example are zip archives (index 0 below).
 compatible_arch = ['Zip', '7-zip', 'gzip', 'bzip2']
