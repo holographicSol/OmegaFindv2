0a834e6cad80c91ffc97a9658c54970960dfcf27
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-04 07:37:17 +0000

    Add files via upload

--- compatible_archives.py
+++ compatible_archives.py
@@ -1,6 +1,8 @@
 # Note: this already accounts for many archive suffixes. an epub for example can be a zip archive (index 0 below).
 compatible_arch = ['Zip', '7-zip', 'gzip', 'bzip2']
 
+# Add beginning of magic buffer to below groups appropriately if needed.
+# If add new group, then also add new method to handler_file.py.
 group_zipfile_compat = ['Zip archive']
 group_py7zr_compat = ['7-zip archive']
 group_tarfile_compat = ['gzip compressed', 'bzip2 compressed']
