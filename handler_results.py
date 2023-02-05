06bc9a35ea17d42d804f93727d76f0863ee60f80
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-05 07:26:50 +0000

    Add files via upload

--- handler_results.py
+++ handler_results.py
@@ -3,7 +3,7 @@ import handler_file
 
 
 def result_handler(_results: list, msg: str):
-    print(f'-- {msg} files:')
+    print(f'\n   {msg}')
     i_result = 0
     for result in _results:
         if i_result <= 3:
@@ -29,21 +29,23 @@ def post_scan_results(_results: list, _db_recognized_files: str, _learn_bool: bo
                 asyncio.run(handler_file.write_scan_results(*_results,
                                                             file='scan_results_de-scan_' + _dt + '.txt',
                                                             _dt=_dt))
-                result_handler(_results=_results, msg='unrecognized')
+                result_handler(_results=_results, msg='[Unrecognized]')
             elif _type_scan_bool is True:
                 print(f'-- found files: {len(_results)}')
                 print('-- writing results ...')
                 asyncio.run(handler_file.write_scan_results(*_results,
                                                             file='scan_results_type-scan_' + _dt + '.txt',
                                                             _dt=_dt))
-                result_handler(_results=_results, msg='found')
+                result_handler(_results=_results, msg='[Found]')
             elif _p_scan is True:
                 print(f'-- found files: {len(_results)}')
                 print('-- writing results ...')
                 asyncio.run(handler_file.write_scan_results(*_results,
                                                             file='scan_results_pscan_' + _dt + '.txt',
                                                             _dt=_dt))
-                result_handler(_results=_results, msg='potentially password protected')
+                result_handler(_results=_results, msg='[PASSWORD PROTECTED]')
+        else:
+            print('-- zero results.')
     else:
         print('-- zero results.')
     print('')
