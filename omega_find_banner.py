6df98b0e6b8902b4a75a0439530b0af729e796fa
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-06 05:15:39 +0000

    Add files via upload

--- omega_find_banner.py
+++ omega_find_banner.py
@@ -2,5 +2,4 @@ def banner():
     print('')
     print('[OmegaFind v2] Multi-processed async for better performance.')
     print('               Forensics tool. Search differently.')
-    print('               Developed and written by Benjamin Jack Cullen.')
     print('')
