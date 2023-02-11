492d8640f03963f506df6713d0db4d362c3ab150
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-11 12:41:42 +0000

    Add files via upload

--- async_check.py
+++ async_check.py
@@ -5,4 +5,4 @@ import handler_strings
 async def scan_check(file: str, suffix: str, buffer: bytes, _recognized_files: list) -> list:
     buffer = handler_strings.sub_str(_buffer=buffer)
     if [suffix, buffer] not in _recognized_files:
-        return [file, suffix, buffer]
+        return [file, buffer]
