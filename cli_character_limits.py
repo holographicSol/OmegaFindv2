1fa30c9223358da774a68decf74bf89cf1ac2db8
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-11 15:34:30 +0000

    Add files via upload

--- cli_character_limits.py
+++ cli_character_limits.py
@@ -5,8 +5,14 @@ import screeninfo
 
 
 def column_width_from_screen_size_using_ratio(n: int) -> int:
+    # print(f'n: {n}')
     w = 0
     for m in screeninfo.get_monitors():
         if m.is_primary is True:
             w = m.width
-    return int(0.058*int(w))
+            # print(f'w: {w}')
+
+    # print(f'suggested column width: {int(0.058*int(w) / int(n))}')
+    return int(0.058*int(w) / int(n))
+
+
