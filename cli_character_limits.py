8b115923d5c500ba8dfaacb52a539f27f446c635
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-11 15:03:53 +0000

    Add files via upload

---
+++ cli_character_limits.py
@@ -0,0 +1,12 @@
+""" Written by Benjamin Jack Cullen """
+
+import tabulate
+import screeninfo
+
+
+def column_width_from_screen_size_using_ratio() -> int:
+    w = 0
+    for m in screeninfo.get_monitors():
+        if m.is_primary is True:
+            w = m.width
+    return int(0.058*int(w))
