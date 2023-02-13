086c60d9cd233bcdfef25da4fcc72c27efaaab33
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-13 16:56:36 +0000

    Add files via upload

--- handler_table_rows.py
+++ handler_table_rows.py
@@ -1,29 +1,37 @@
+import handler_print
+
 
 def display_rows_interactively(max_limit: int, _results: list, table: str):
 
-    if len(_results) > max_limit:
-        i_limiter = 0
-        p = ''
-        for char in table:
+    try:
+        if len(_results) > max_limit:
+            i_limiter = 0
+            p = ''
+            for char in table:
 
-            if char == '\n':
-                if i_limiter <= max_limit:
-                    print(p)
-                    i_limiter += 1
+                if char == '\n':
+                    if i_limiter <= max_limit:
+                        print(p)
+                        i_limiter += 1
+                    else:
+                        input('\n--- more ---\n')
+                        i_limiter = 0
+                    p = ''
+
+                elif char != '\n':
+                    p = p + char
+
                 else:
-                    input('\n--- more ---\n')
-                    i_limiter = 0
-                p = ''
+                    if i_limiter <= max_limit:
+                        print(p)
+                        i_limiter += 1
+                    else:
+                        input('\n--- more ---\n')
+                        i_limiter = 0
 
-            elif char != '\n':
-                p = p + char
-
-            else:
-                if i_limiter <= max_limit:
-                    print(p)
-                    i_limiter += 1
-                else:
-                    input('\n--- more ---\n')
-                    i_limiter = 0
-    else:
-        print(table)
+        else:
+            print(table)
+            
+    except KeyboardInterrupt:
+        handler_print.display_spacer()
+        pass
