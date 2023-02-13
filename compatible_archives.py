20254e2e255369045aeaef81eb2a07653f218b9c
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-13 17:04:02 +0000

    Delete compatible_archives.py

--- compatible_archives.py
+++
@@ -1,8 +0,0 @@
-# Note: this already accounts for many archive suffixes. an epub for example can be a zip archive (index 0 below).
-compatible_arch = ['Zip archive', '7-zip archive', 'gzip compressed', 'bzip2 compressed']
-
-# Add beginning (concise) or full (strict) result of magic buffer to below groups appropriately if needed.
-# If add new group, then also add new method to handler_file.py.
-group_zipfile_compat = ['Zip archive']
-group_py7zr_compat = ['7-zip archive']
-group_tarfile_compat = ['gzip compressed', 'bzip2 compressed']
