7b1d147cd423ea060216b0b10c0545cb31bf0433
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-14 15:28:51 +0000

    Add files via upload

--- tabulate_helper.py
+++ tabulate_helper.py
@@ -4,6 +4,26 @@ Compliment tabulate module.
 Overcome potential Terminal/Console buffer limitations causing a table to not be entirely displayed.
 Display N rows of table at a time, with some customization.
 
+Simple Example:
+tabulate_helper.display_rows_interactively(max_limit=75,
+                                           results=_results,
+                                           table=table_1,
+                                           extra_input=False,
+                                           message='\n--- more ---\n',
+                                           function=None)
+Note: Simply display rows until max_limit reached and display message.
+
+Advanced Example:
+tabulate_helper.display_rows_interactively(max_limit=75,
+                                           results=a_list,
+                                           table=a_table,
+                                           extra_input=True,
+                                           message='\n--- more ---\n',
+                                           function=a_module.a_function_that_takes_input)
+
+Note: a_function_that_takes_input may simply take digits that point at an index in results and
+do something.
+
 """
 
 import handler_print
@@ -56,3 +76,18 @@ def display_rows_interactively(max_limit: int, results: list, table: str, extra_
     except KeyboardInterrupt:
         handler_print.display_spacer()
         pass
+
+
+"""
+Example:
+
+tabulate_helper.display_rows_interactively(max_limit=75,
+                                           results=a_list,
+                                           table=a_table,
+                                           extra_input=True,
+                                           message='\n--- more ---\n',
+                                           function=a_module.a_function_that_takes_input)
+
+Note: a_function_that_takes_input may simply take digits that point at an index in results and
+do something.
+"""
