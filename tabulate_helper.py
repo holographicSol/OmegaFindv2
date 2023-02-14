8dcff8dd1524a021c64f3e2b98d342611c94ef73
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-14 15:10:57 +0000

    Add files via upload

--- tabulate_helper.py
+++ tabulate_helper.py
@@ -1,4 +1,10 @@
-""" Written by Benjamin Jack Cullen """
+""" Written by Benjamin Jack Cullen
+
+Compliment tabulate module.
+Overcome potential Terminal/Console buffer limitations causing a table to not be entirely displayed.
+Display N rows of table at a time, with some customization.
+
+"""
 
 import handler_print
 
