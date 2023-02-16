6c3c1707107af0f72ba236256513bc0bc0e2295d
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-16 06:40:34 +0000

    Add files via upload

--- cli_character_limits.py
+++ cli_character_limits.py
@@ -1,4 +1,5 @@
 """ Written by Benjamin Jack Cullen """
+
 import os
 import subprocess
 import screeninfo
@@ -6,7 +7,6 @@ import screeninfo
 
 def column_width_from_screen_size_using_ratio(n: int, reduce=0, add=0, ratio=0.134) -> int:
     """ Default ratio defined for font size 12. """
-
     w = 64
     for m in screeninfo.get_monitors():
         if m.is_primary is True:
@@ -14,13 +14,11 @@ def column_width_from_screen_size_using_ratio(n: int, reduce=0, add=0, ratio=0.1
     return add_sub(n=int(int(w * ratio) / set_n(n)), reduce=reduce, add=add)
 
 
-def column_width_from_screen_size_using_os_get_terminal_size(n: int, reduce=0, add=0) -> int:
-
+def column_width_from_os_get_terminal_size(n: int, reduce=0, add=0) -> int:
     return add_sub(n=int(int(os.get_terminal_size().columns) / set_n(n)), reduce=reduce, add=add)
 
 
-def column_width_from_screen_size_using_tput(n: int, reduce=0, add=0) -> int:
-
+def column_width_from_tput(n: int, reduce=0, add=0) -> int:
     w = int(int(int(subprocess.Popen(['tput', 'cols'], stdout=subprocess.PIPE).communicate()[0].strip())) / set_n(n))
     return add_sub(n=w, reduce=reduce, add=add)
 
