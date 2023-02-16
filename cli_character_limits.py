7bc9a03bb8cc75e48fbf905ba9ffacf1ffbef6f1
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-02-16 06:35:34 +0000

    Add files via upload

--- cli_character_limits.py
+++ cli_character_limits.py
@@ -23,8 +23,7 @@ def column_width_from_screen_size_using_os_get_terminal_size(n: int, reduce=0, a
 
 def column_width_from_screen_size_using_tput(n: int, reduce=0, add=0) -> int:
 
-    tput = subprocess.Popen(['tput', 'cols'], stdout=subprocess.PIPE)
-    w = int(int(int(tput.communicate()[0].strip())) / set_n(n))
+    w = int(int(int(subprocess.Popen(['tput', 'cols'], stdout=subprocess.PIPE).communicate()[0].strip())) / set_n(n))
     w = add_sub(n=w, reduce=reduce, add=add)
     return w
 
