8324cbb9062bcc41fec0cbaad00d277f53b105f0
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-04-22 17:36:50 +0100

    Add files via upload

--- handler_chunk.py
+++ handler_chunk.py
@@ -10,10 +10,12 @@ def chunk_data(data: list, chunk_size: int) -> list:
 
 
 def un_chunk_data(data: list, depth: int) -> list:
+    # with large amounts of data this can take a moment to complete. todo: re-work this for performance.
     new_data = data
     for i in range(0, depth):
         new_sub_data = []
         for dat in new_data:
+            # print(dat)
             if dat is not None:
                 for x in dat:
                     if x is not None:
