8e9adf1d3a02aa327ce42ca229cecaffb286cbcc
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-04-23 17:22:24 +0100

    Add files via upload

--- handler_convert_results.py
+++ handler_convert_results.py
@@ -18,8 +18,9 @@ def convert_results(_results, _human_size=False, _mtime_scan=False):
 def convert_string_match_results(_results, _human_size=False):
     n_result = 0
     for sublist in _results:
+        print(sublist)
         sublist[1] = handler_file.convert_timestamp_to_datetime(float(sublist[1]))
         if _human_size is True:
-            sublist[1] = str(power_converter.convert_bytes(*[int(sublist[1])], abbr=True)[0])
+            sublist[2] = str(power_converter.convert_bytes(*[int(sublist[2])], abbr=True)[0])
         n_result += 1
     return _results
