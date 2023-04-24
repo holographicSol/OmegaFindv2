105b2a554051146b8a8be7f69fde096a9d656dee
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-04-24 01:00:26 +0100

    Add files via upload

--- async_mtimescan.py
+++ async_mtimescan.py
@@ -11,56 +11,60 @@ async def entry_point_mtime_scan(chunk: list, **kwargs) -> list:
     _target = str(kwargs.get('target'))
     _program_root = str(kwargs.get('program_root'))
     _extract = kwargs.get('extract')
-    human_size = kwargs.get('human_size')
+    _human_size = kwargs.get('human_size')
 
     if _extract is False:
-        return [await mtime_scan(item, _extract, _target, _program_root, human_size) for item in chunk]
+        return [await mtime_scan(_file=item, _extract=_extract, _target=_target, _program_root=_program_root,
+                                 _human_size=_human_size) for item in chunk]
     elif _extract is True:
-        return [await mtime_scan_extract(item, _extract, _target, _program_root, human_size) for item in chunk]
+        return [await mtime_scan_extract(_file=item, _extract=_extract, _target=_target, _program_root=_program_root,
+                                         _human_size=_human_size) for item in chunk]
 
 
-async def mtime_scan(file: str, _extract: bool, _target: str, _program_root: str, human_size=False) -> list:
+async def mtime_scan(_file: str, _extract: bool, _target: str, _program_root: str, _human_size=False) -> list:
     _result = []
     try:
-        m = await asyncio.to_thread(handler_file.get_m_time, file)
-        s = await asyncio.to_thread(handler_file.get_size, file, human_size)
-        # print(m, s, file)
-        return [m, s, file]
+        # todo
+        m = await asyncio.to_thread(handler_file.get_m_time, _file)
+        s = await asyncio.to_thread(handler_file.get_size, _file, _human_size)
+        return [m, s, _file]
 
     except Exception as e:
-        _result = [['[ERROR]', str(file), str(e)]]
+        _result = [['[ERROR]', str(_file), str(e)]]
     return _result
 
 
-async def mtime_scan_extract(file: str, _extract: bool, _target: str, _program_root: str, human_size=False) -> list:
+async def mtime_scan_extract(_file: str, _extract: bool, _target: str, _program_root: str, _human_size=False) -> list:
     _result = []
     try:
-        _result = await extract_mtime_scan(_file=file, _target=_target, _program_root=_program_root,
-                                           human_size=human_size)
+        _result = await extract_mtime_scan(_file=_file, _target=_target, _program_root=_program_root,
+                                           _human_size=_human_size)
     except Exception as e:
-        _result = [['[ERROR]', str(file), str(e)]]
+        _result = [['[ERROR]', str(_file), str(e)]]
     return _result
 
 
-async def extract_mtime_scan(_file: str, _target: str, _program_root: str, human_size=False) -> list:
+async def extract_mtime_scan(_file: str, _target: str, _program_root: str, _human_size=False) -> list:
     _results = []
+    # todo
     m = await asyncio.to_thread(handler_file.get_m_time, _file)
-    s = await asyncio.to_thread(handler_file.get_size, _file, human_size)
+    s = await asyncio.to_thread(handler_file.get_size, _file, _human_size)
     _results = [[m, s, _file]]
     _tmp = _program_root+'\\tmp\\'+str(handler_strings.randStr())
     result_bool, extraction = await asyncio.to_thread(handler_extraction_method.extract_nested_compressed,
                                                       file=_file, temp_directory=_tmp, _target=_target,
                                                       _static_tmp=_tmp)
     if result_bool is True:
+        # todo
         sub_files = await asyncio.to_thread(scanfs.scan, _tmp)
-        # sub_files = await asyncio.to_thread(handler_chunk.un_chunk_data, sub_files, depth=1)
         sub_files[:] = [item for sublist in sub_files for item in sublist]
         for sub_file in sub_files:
+            # todo
             m = await asyncio.to_thread(handler_file.get_m_time, sub_file)
-            s = await asyncio.to_thread(handler_file.get_size, sub_file, human_size)
+            s = await asyncio.to_thread(handler_file.get_size, sub_file, _human_size)
             res = [m, s, sub_file]
             if res is not None:
-                res[2] = res[2].replace(str(_tmp), _target)
+                res[2] = res[2].replace(str(_tmp), _file)
                 _results.append(res)
     await asyncio.to_thread(handler_file.rem_dir, path=_tmp)
     return _results
