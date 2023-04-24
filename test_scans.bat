1c9086dda0e69cd310ab37d9fc506d0ae5a9d29b
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-04-24 02:33:17 +0100

    Add files via upload

--- test_scans.bat
+++ test_scans.bat
@@ -1,23 +1,23 @@
-python .\omega_find.py -R -r ".\test_files\"
+python .\omega_find.py -cmax 1 -R -cmax 1 -r ".\test_files\"
 
-python .\omega_find.py -R -e -r ".\test_files\"
+python .\omega_find.py -cmax 1 -R -e -cmax 1 -r ".\test_files\"
 
-python .\omega_find.py -R -d ".\test_files\"
+python .\omega_find.py -cmax 1 -R -d ".\test_files\"
 
-python .\omega_find.py -R -e -d ".\test_files\"
+python .\omega_find.py -cmax 1 -R -e -d ".\test_files\"
 
-python .\omega_find.py -R -p ".\test_files\"
+python .\omega_find.py -cmax 1 -R -p ".\test_files\"
 
-python .\omega_find.py -R -t .\test_files\ -gsfx text
+python .\omega_find.py -cmax 1 -R -t .\test_files\ -gsfx text
 
-python .\omega_find.py -R -e -t .\test_files\ -gsfx text
+python .\omega_find.py -cmax 1 -R -e -t .\test_files\ -gsfx text
 
-python .\omega_find.py -R -c .\test_files\ -q hello
+python .\omega_find.py -cmax 1 -R -c .\test_files\ -q hello
 
-python .\omega_find.py -R -e -c .\test_files\ -q hello
+python .\omega_find.py -cmax 1 -R -e -c .\test_files\ -q hello
 
-python .\omega_find.py -R -m ".\test_files\"
+python .\omega_find.py -cmax 1 -R -m ".\test_files\"
 
-python .\omega_find.py -R -e -m ".\test_files\"
+python .\omega_find.py -cmax 1 -R -e -m ".\test_files\"
 
-python .\omega_find.py -R -s .\test_files\ -q .
+python .\omega_find.py -cmax 1 -R -s .\test_files\ -q .
---
+++ test_scans_compiled.cmd
@@ -0,0 +1,23 @@
+"./omega_find.exe" -cmax 1 -R -cmax 1 -r ".\test_files\"
+
+"./omega_find.exe" -cmax 1 -R -e -cmax 1 -r ".\test_files\"
+
+"./omega_find.exe" -cmax 1 -R -d ".\test_files\"
+
+"./omega_find.exe" -cmax 1 -R -e -d ".\test_files\"
+
+"./omega_find.exe" -cmax 1 -R -p ".\test_files\"
+
+"./omega_find.exe" -cmax 1 -R -t .\test_files\ -gsfx text
+
+"./omega_find.exe" -cmax 1 -R -e -t .\test_files\ -gsfx text
+
+"./omega_find.exe" -cmax 1 -R -c .\test_files\ -q hello
+
+"./omega_find.exe" -cmax 1 -R -e -c .\test_files\ -q hello
+
+"./omega_find.exe" -cmax 1 -R -m ".\test_files\"
+
+"./omega_find.exe" -cmax 1 -R -e -m ".\test_files\"
+
+"./omega_find.exe" -cmax 1 -R -s .\test_files\ -q .
