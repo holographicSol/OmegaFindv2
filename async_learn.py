b2afcad8bf58c6c5f06d590b35dc5df1ea3fe37e
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2023-04-26 00:19:28 +0100

    Add files via upload

--- async_learn.py
+++ async_learn.py
@@ -18,9 +18,9 @@ async def entry_point_learn(chunk: list, **kwargs) -> list:
 async def scan_learn_check(_suffix: str, _buffer: bytes, _recognized_files: list) -> list:
     global x_learn
     assoc = [_suffix, _buffer]
-    if await async_check.check_list(_list_1=assoc, _list_2=x_learn) is False:
+    if await async_check.check_not_in_list(_list_1=assoc, _list_2=x_learn) is False:
         x_learn.append(assoc)
-        if await async_check.check_list(_list_1=assoc, _list_2=_recognized_files) is False:
+        if await async_check.check_not_in_list(_list_1=assoc, _list_2=_recognized_files) is False:
             return assoc
 
 
@@ -32,3 +32,30 @@ async def scan_learn(_file: str, _recognized_files: list, _buffer_max: int) -> l
     except Exception as e:
         _result = [['[ERROR]', str(_file), str(e)]]
     return _result
+
+
+# async def extract_type_scan(_file: str, _recognized_files: list,  _buffer_max: int,
+#                             _type_suffix: list, _target: str, _program_root: str, _digits=True,
+#                             _human_size=False) -> list:
+#
+#     _results = [await type_scan(_file=_file, _recognized_files=_recognized_files, _buffer_max=_buffer_max,
+#                                 _type_suffix=_type_suffix, _target=_target, _program_root=_program_root,
+#                                 _digits=_digits, _human_size=_human_size)]
+#
+#     _tmp = _program_root+'\\tmp\\'+str(handler_strings.randStr())
+#     result_bool, extraction = await asyncio.to_thread(handler_extraction_method.extract_nested_compressed,
+#                                                       file=_file, temp_directory=_tmp, _target=_target,
+#                                                       _static_tmp=_tmp)
+#     if result_bool is True:
+#         sub_files = await asyncio.to_thread(scanfs.scan, _path=_tmp)
+#         sub_files[:] = [item for sublist in sub_files for item in sublist]
+#         for sub_file in sub_files:
+#             res = await type_scan(_file=sub_file, _recognized_files=_recognized_files, _buffer_max=_buffer_max,
+#                                   _type_suffix=_type_suffix, _target=_target, _program_root=_program_root,
+#                                   _digits=_digits, _human_size=_human_size)
+#             if res is not None:
+#                 if len(res) == 4:
+#                     res[3] = res[3].replace(str(_tmp), _file)
+#                     _results.append(res)
+#     await asyncio.to_thread(handler_file.rem_dir, path=_tmp)
+#     return _results
