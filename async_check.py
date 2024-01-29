8a5a427a23db1ed0cee5c8fed24c4740c4ced038
holographic_sol <88397088+holographicSol@users.noreply.github.com>
2024-01-29 09:32:46 +0000

    Update async_check.py

--- async_check.py
+++ async_check.py
@@ -5,13 +5,13 @@ import asyncio
 import handler_file
 
 
-async def check_in_list(_list_1: list, _list_2: list) -> bool:
+async def check_not_in_list(_list_1: list, _list_2: list) -> bool:
     if _list_1 not in _list_2:
         return False
 
 
-async def check_not_in_list(_list_1: list, _list_2: list) -> bool:
-    if _list_1 not in _list_2:
+async def check_in_list(_list_1: list, _list_2: list) -> bool:
+    if _list_1 in _list_2:
         return False
 
 
@@ -20,8 +20,8 @@ async def scan_check(_file: str, _suffix: str, _buffer: bytes, _recognized_files
     sub_buffer = _buffer
     if _digits is False:
         sub_buffer = await asyncio.to_thread(handler_strings.sub_str, _buffer=_buffer)
-    assoc = [_suffix, _buffer]
-    if await check_in_list(_list_1=assoc, _list_2=sub_buffer) is False:
+    assoc = [_suffix, sub_buffer]
+    if await check_not_in_list(_list_1=assoc, _list_2=_recognized_files) is False:
         m = await asyncio.to_thread(handler_file.get_m_time, _file=_file)
         s = await asyncio.to_thread(handler_file.get_size, _file)
         return [m, _buffer, s, _file]
@@ -33,7 +33,7 @@ async def type_scan_check(_file: str, _buffer: bytes, _recognized_files: list, _
     if _digits is False:
         sub_buffer = await asyncio.to_thread(handler_strings.sub_str, _buffer=_buffer)
     assoc = [sub_buffer]
-    if await check_not_in_list(_list_1=assoc, _list_2=sub_buffer) is False:
+    if await check_in_list(_list_1=assoc, _list_2=_recognized_files) is False:
         m = await asyncio.to_thread(handler_file.get_m_time, _file=_file)
         s = await asyncio.to_thread(handler_file.get_size, _file=_file)
         return [m, _buffer, s, _file]
